98c8388a55eb864a8c9ff6ca4cf5c6288723d20f
siddhaarth-tech <siddhaarth@mobicip.com>
2026-08-05 19:33:14 +0530

    Create String1.swift

---
+++ StringandChar/String1.swift
@@ -0,0 +1,48 @@
+// Single-line string
+let city = "Tokyo"
+print(city)
+
+// Multiline string
+let note = """
+Good Morning
+Have a nice day
+"""
+print(note)
+
+// Double quotes
+let quote = "He said \"Hello\""
+print(quote)
+
+// Backslash
+let path = "C:\\Users\\Alex"
+print(path)
+
+// New line
+let lines = "One\nTwo"
+print(lines)
+
+// Tab
+let table = "Name\tAge"
+print(table)
+
+// Carriage return
+let word = "Apple\rOrange"
+print(word)
+
+// Unicode
+let heart = "\u{2764}"
+print(heart)
+
+// Extended string delimiter
+let text1 = #"One\nTwo"#
+print(text1)
+
+// Extended delimiter with escape
+let text2 = #"One\#nTwo"#
+print(text2)
+
+// Multiline with """
+let marks = #"""
+Triple quotes: """
+"""#
+print(marks)
