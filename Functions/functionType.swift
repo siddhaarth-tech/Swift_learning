6fa44537f974e3a7d0838dd534f602ee36b40972
siddhaarth-tech <siddhaarth@mobicip.com>
2026-08-10 21:01:21 +0530

    Create functionType.swift

---
+++ Functions/functionType.swift
@@ -0,0 +1,20 @@
+func addOp(_ a:Int,_ b: Int) -> Int{
+  return a+b
+}
+
+func multiplyOp(_ a:Int,_ b: Int) -> Int{
+  return a*b
+}
+func increment(_ a:Int) -> Int{
+  return a+1
+}
+//var operations : (Int ,Int)->Int ---> defining the function type 
+var operations = addOp
+print(operations(27,3))
+operations = multiplyOp
+print(operations(27,3))
+
+//here operations = operationForOne is not able to be used for the increment as they have different fucntion type so new variable must be used to assign that type 
+
+var operationForOne = increment
+print(operationForOne(27))
