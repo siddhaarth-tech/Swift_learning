20c5996f67e66bea566046f9966f9e6cf1ebccf5
siddhaarth-tech <siddhaarth@mobicip.com>
2026-08-12 20:31:33 +0530

    Add Student class with properties and example usage

---
+++ structAndClasses/Class.swift
@@ -0,0 +1,15 @@
+class Student {
+    var name = "Siddhaarth"
+    var age = 20
+}
+
+var student1 = Student()
+
+print(student1.name)
+print(student1.age)
+
+student1.name = "Sid"
+student1.age = 21
+
+print(student1.name)
+print(student1.age)
