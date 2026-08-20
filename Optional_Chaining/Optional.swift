60e7012a057cf782d157fcd561917e981538b5eb
siddhaarth-tech <siddhaarth@mobicip.com>
2026-08-20 20:16:56 +0530

    Implement Person and Role classes with optional property
    Added classes Person and Role with optional property.

---
+++ Optional_Chaining/Optional.swift
@@ -0,0 +1,14 @@
+class Person{
+    var name : String = "Siddhaarth"
+}
+class Role{
+    var occupation : String?
+}
+
+var Person1 = Person()
+var Role1 = Role()
+
+Person1.name = "Sidd"
+
+print(Person1.name)
+print(Role1.occupation)
